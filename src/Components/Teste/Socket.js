da7f606249f4ee2857cd918351e07bc40295142f
iagoguimaraes <iagog6@hotmail.com>
2019-10-05 00:18:08 -0300

    implementação de funcionalidades no socket tester

--- src/Components/Teste/Socket.js
+++ src/Components/Teste/Socket.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Row, Col, Form, InputGroup, Button, Card, ListGroup, Tab, Tabs, Alert } from 'react-bootstrap'
+import { Container, Row, Col, Form, InputGroup, Button, Card, ListGroup, Tab, Tabs, Badge } from 'react-bootstrap'
 import socketIOClient from "socket.io-client";
 
 class Socket extends React.Component {
@@ -7,15 +7,24 @@ class Socket extends React.Component {
     constructor() {
         super();
         this.state = {
-            host: "localhost:3002",
-            opt: { opt1: "opt1" },
-            listen: ["listen1", "listen2"],
+            status: "Desconectado",
+            host: "http://localhost:3002",
+            opt: '{ opt1: "opt1" }',
+            listen: ["listen1", "listen2", "listen3"],
             emit: ["emit1", "emit2"],
-            history: [],
-            emitBody: {
+            history: [
+                { header: "[22:05] Listen 1", body: '{"key": "value"}' },
+                { header: "[22:01] Listen 3", body: '{"key": "value"}' },
+                { header: "[22:00] Listen 2", body: '{"key": "value"}' }
+            ],
+            emitText: {
                 "emit1": "{'emit1': 'emit1'}",
                 "emit2": "{'emit2': 'emit2'}"
-            }
+            },
+            addListen: "",
+            addEmit: "",
+            emitSelected: "",
+            emitSelectedText: "",
         };
     }
 
@@ -27,20 +36,61 @@ class Socket extends React.Component {
         //this.socket.emit("mesa/cadastrar", { n: this.state.formulario.n_mesa });
     }
 
+    limparHistorico = (event) => {
+        this.setState({ history: [] });
+    }
+
+    adicionarListen = (listen) => {
+        this.setState({ listen: [...this.state.listen, this.state.addListen] });
+    }
+
+    removerListen = (listen) => {
+        let filteredArray = this.state.listen.filter(item => item !== listen)
+        this.setState({ listen: filteredArray });
+    }
+
+    adicionarEmit = (event) => {
+        this.setState({ emit: [...this.state.emit, this.state.addEmit] });
+    }
+
+    removerEmit = (emit) => {
+        let filteredArray = this.state.emit.filter(item => item !== emit)
+        this.setState({ emit: filteredArray });
+    }
+
     formChange = (event) => {
-        let formNewState = Object.assign({}, this.state.formulario);
-        formNewState[event.target.name] = event.target.value;
-        this.setState({ formulario: formNewState });
+        this.setState({ [event.target.name]: event.target.value });
+    }
+
+    handleKeyDown(event) {
+        if (event.keyCode === 9) { // tab was pressed
+            event.preventDefault();
+            var val = this.state.emitSelectedText,
+                start = event.target.selectionStart,
+                end = event.target.selectionEnd;
+
+            this.setState({ "emitSelectedText": val.substring(0, start) + '\t' + val.substring(end) }, () => this.refs.input.selectionStart = this.refs.input.selectionEnd = start + 1);
+        }
     }
 
     render() {
         return (
-            <Container>
+            <Container style={{ maxWidth: "100%" }}>
                 <Row style={{ marginTop: 10 }}>
                     <Col xs={12}>
                         <InputGroup>
-                            <Form.Control aria-describedby="basic-addon1" placeholder="http://localhost:80" />
-                            <Form.Control aria-describedby="basic-addon1" placeholder="{token: 'token'}" />
+                            <Form.Control
+                                aria-describedby="basic-addon1"
+                                placeholder="http://localhost:80"
+                                name="host"
+                                onChange={this.formChange}
+                                value={this.state.host} />
+                            <Form.Control
+                                aria-describedby="basic-addon1"
+                                placeholder="{token: 'token'}"
+                                name="opt"
+                                onChange={this.formChange}
+                                value={this.state.opt} />
                             <InputGroup.Append>
                                 <Button variant="outline-success">Conectar</Button>
                                 <Button variant="outline-danger">Desconectar</Button>
@@ -57,17 +107,61 @@ class Socket extends React.Component {
                                 <Tabs defaultActiveKey="listen">
                                     <Tab eventKey="listen" title="Listen">
                                         <br></br>
+                                        <InputGroup.Append>
+                                            <Form.Control
+                                                aria-describedby="basic-addon1"
+                                                placeholder="Evento Listen"
+                                                name="addListen"
+                                                onChange={this.formChange}
+                                                value={this.state.addListen} />
+                                            <Button
+                                                variant="outline-secondary"
+                                                onClick={this.adicionarListen}>Add</Button>
+                                        </InputGroup.Append>
+                                        <br></br>
                                         <ListGroup variant="flush">
-                                            <ListGroup.Item>Listen 1</ListGroup.Item>
-                                            <ListGroup.Item>Listen 2</ListGroup.Item>
-                                            <ListGroup.Item>Listen 3</ListGroup.Item>
+                                            {
+                                                this.state.listen.map(obj => {
+                                                    return (
+                                                        <ListGroup.Item>{obj}
+                                                            <Badge
+                                                                variant="light"
+                                                                style={{ float: "right" }}
+                                                                onClick={() => this.removerListen(obj)}>x</Badge>
+                                                        </ListGroup.Item>
+                                                    )
+                                                })
+                                            }
                                         </ListGroup>
                                     </Tab>
                                     <Tab eventKey="emit" title="Emit">
                                         <br></br>
+                                        <InputGroup.Append>
+                                            <Form.Control
+                                                aria-describedby="basic-addon1"
+                                                placeholder="Evento Emit"
+                                                name="addEmit"
+                                                onChange={this.formChange}
+                                                value={this.state.addEmit} />
+                                            <Button
+                                                variant="outline-secondary"
+                                                onClick={this.adicionarEmit}>Add</Button>
+                                        </InputGroup.Append>
+                                        <br></br>
                                         <ListGroup variant="flush">
-                                            <ListGroup.Item>Emit 1</ListGroup.Item>
-                                            <ListGroup.Item>Emit 2</ListGroup.Item>
+                                            {
+                                                this.state.emit.map(obj => {
+                                                    return (
+                                                        <ListGroup.Item onClick={() => this.setState({ emitSelected: obj, emitSelectedText: this.state.emitText[obj] })}>
+                                                            {obj}
+                                                            <Badge
+                                                                variant="light"
+                                                                style={{ float: "right" }}
+                                                                onClick={() => this.removerEmit(obj)}>x</Badge>
+                                                        </ListGroup.Item>
+                                                    )
+                                                })
+                                            }
                                         </ListGroup>
                                     </Tab>
                                 </Tabs>
@@ -79,33 +173,25 @@ class Socket extends React.Component {
                             <Card.Body>
                                 <Card.Title>
                                     Histórico
-                                    <Button variant="secondary" size="sm" className="float-right">Limpar</Button>
+                                    <Button
+                                        variant="secondary"
+                                        size="sm"
+                                        className="float-right"
+                                        onClick={this.limparHistorico}>Limpar</Button>
                                 </Card.Title>
-                                
-                                <br></br>
-
-                                <Card bg="light">
-                                    <Card.Header>[22:05] Liten 2</Card.Header>
-                                    <Card.Body>
-                                        <Card.Text>
-                                            Some quick example text to build on the card title and make up the bulk
-                                            of the card's content.
-                                        </Card.Text>
-                                    </Card.Body>
-                                </Card>
-
-                                <br></br>
-
-                                <Card bg="light">
-                                    <Card.Header>[22:00] Liten 1</Card.Header>
-                                    <Card.Body>
-                                        <Card.Text>
-                                            Some quick example text to build on the card title and make up the bulk
-                                            of the card's content.
-                                        </Card.Text>
-                                    </Card.Body>
-                                </Card>
-
+                                {
+                                    this.state.history.map(obj => {
+                                        return (
+                                            <div>
+                                                <br></br>
+                                                <Card bg="light">
+                                                    <Card.Header>{obj.header}</Card.Header>
+                                                    <Card.Body>{obj.body}</Card.Body>
+                                                </Card>
+                                            </div>
+                                        )
+                                    })
+                                }
                             </Card.Body>
                         </Card>
                     </Col>
@@ -114,18 +200,34 @@ class Socket extends React.Component {
                             <Card.Body>
                                 <Card.Title>Emitir</Card.Title>
                                 <InputGroup>
-                                    <InputGroup.Append>
-                                        <Form.Control aria-describedby="basic-addon1" placeholder="Evento" />
-                                        <Button variant="outline-secondary">Enviar</Button>
-                                    </InputGroup.Append>
+                                    <Col xs={12}>
+                                        <InputGroup.Append>
+                                            <Form.Control
+                                                aria-describedby="basic-addon1"
+                                                placeholder="Evento"
+                                                name="emitSelected"
+                                                onChange={this.formChange}
+                                                value={this.state.emitSelected} />
+                                            <Button variant="outline-secondary">Enviar</Button>
+                                        </InputGroup.Append>
+                                        <br></br>
+                                        <Form.Control
+                                            as="textarea"
+                                            rows="10"
+                                            ref="input"
+                                            name="emitSelectedText"
+                                            onKeyDown={this.handleKeyDown.bind(this)}
+                                            onChange={this.formChange}
+                                            value={this.state.emitSelectedText}>
+                                        </Form.Control>
+                                    </Col>
                                 </InputGroup>
-                                <br></br>
-                                <textarea></textarea>
+
                             </Card.Body>
                         </Card>
                     </Col>
                 </Row>
-            </Container>
+            </Container >
         )
     }
 }
