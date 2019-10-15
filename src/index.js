ac325952c46d0b9ea75e6a5ed24d3a6404c3dd59
iagoguimaraes <iagog6@hotmail.com>
2019-10-15 18:52:44 -0300

    remove all

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import * as serviceWorker from './serviceWorker';
-import Route from './routes';
-
-ReactDOM.render(<Route/>, document.getElementById('root'));
-serviceWorker.unregister();
