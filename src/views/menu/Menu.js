85e43fb1f28bdf97991175d7cc964a7795d94708
Brendon Guedes <lordbguedes@gmail.com>
2019-10-24 17:47:15 -0300

    filtro menu ativo fora da lista

--- src/views/menu/Menu.js
+++ src/views/menu/Menu.js
@@ -1,9 +1,23 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import ListaMenu from './ListaMenu';
-import { Card, CardHeader, CardBody, Button } from 'reactstrap';
+import { Card, CardHeader, CardBody, Button, InputGroup, Label, FormGroup } from 'reactstrap';
+import { AppSwitch } from '@coreui/react'
 
 class Produto extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      showVisivel: "",
+    };
+  }
+
+  changeSwitch = (event) => {
+    this.setState({ [event.target.name]: event.target.checked ? 1 : 0 });
+  }
+
   render() {
     return (
       <div>
@@ -19,7 +33,22 @@ class Produto extends Component {
             </div>
           </CardHeader>
           <CardBody>
-            <ListaMenu></ListaMenu>
+            
+            <FormGroup className="mt-4">
+              <InputGroup>
+                <Label>Mostrar menus inativos:</Label>
+                <AppSwitch
+                  name="showVisivel"
+                  className={'mx-3'}
+                  variant={'pill'}
+                  color={'success'}
+                  checked={this.state.showVisivel ? true : false}
+                  onChange={this.changeSwitch}
+                />
+
+              </InputGroup>
+            </FormGroup>
+            <ListaMenu showVisivel={this.state.showVisivel}></ListaMenu>
           </CardBody>
         </Card>
       </div>
