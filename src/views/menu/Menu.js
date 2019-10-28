bbea630d803f1364ef8b7afca7b417844d9a19b4
Brendon Guedes <lordbguedes@gmail.com>
2019-10-28 11:54:52 -0300

    Correções básicas de placeholder

--- src/views/menu/Menu.js
+++ src/views/menu/Menu.js
@@ -155,7 +155,7 @@ class ListaMenu extends Component {
           </Modal.Header>
 
           <Modal.Body>
-            <p>Você tem certeza que deseja excluir ?</p>
+            <p>Você tem certeza que deseja excluir?</p>
           </Modal.Body>
 
           <Modal.Footer>
