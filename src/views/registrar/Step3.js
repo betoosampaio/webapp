e017e8a3bc29cddaa96b0a2a20af0115bee0df75
Brendon Guedes <lordbguedes@gmail.com>
2019-10-29 12:30:20 -0300

    feedbackform em registrar step3

--- src/views/registrar/Step3.js
+++ src/views/registrar/Step3.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
-import { Form, FormGroup, Label, InputGroup, InputGroupAddon, InputGroupText, Input, Button, FormFeedback  } from 'reactstrap';
+import { Form, FormGroup, Label, InputGroup, InputGroupAddon, InputGroupText, Input, Button, FormFeedback } from 'reactstrap';
 import MaskedInput from 'react-text-mask';
 import SelectBanco from '../../components/selectBanco/SelectBanco';
 import { AppSwitch } from '@coreui/react';
+import serverRequest from '../../utils/serverRequest';
+
 
 const stateName = "Step3";
 
@@ -19,8 +21,169 @@ class Step3 extends Component {
       conta: "",
       digito: "",
       cpfcnpj_conta: "",
+
+      validacao: {
+        cpf_administrador: { ok: true, msg: '' },
+        cnpj: { ok: true, msg: '' }
+      }
     }
+  };
+
+  validarCPF = (event) => {
+    let ok = false, msg = '';
+    let val = event.target.value.replace(/\D/g, '');
+    if (!val) {
+      msg = 'Campo obrigatório';
+    }
+    else if (val.length < 11) {
+      msg = 'Formato inválido';
+    }
+    else if (!this.testarCPF(val)) {
+      msg = 'CPF incorreto';
+    }
+    else if (val.toString().includes('11111111111')) {
+      msg = 'Formato inválido';
+    }
+    else if (val.toString().includes('22222222222')) {
+      msg = 'Formato inválido';
+    }
+    else if (val.toString().includes('33333333333')) {
+      msg = 'Formato inválido';
+    }
+    else if (val.toString().includes('44444444444')) {
+      msg = 'Formato inválido';
+    }
+    else if (val.toString().includes('55555555555')) {
+      msg = 'Formato inválido';
+    }
+    else if (val.toString().includes('66666666666')) {
+      msg = 'Formato inválido';
+    }
+    else if (val.toString().includes('77777777777')) {
+      msg = 'Formato inválido';
+    }
+    else if (val.toString().includes('88888888888')) {
+      msg = 'Formato inválido';
+    }
+    else if (val.toString().includes('99999999999')) {
+      msg = 'Formato inválido';
+    }
+    else {
+      ok = true;
+    }
+
+    let newState = Object.assign({}, this.state.validacao);
+    newState.cpf_administrador.ok = ok;
+    newState.cpf_administrador.msg = msg;
+    this.setState({ validacao: newState });
   }
+
+  testarCPF = (strCPF) => {
+    var Soma;
+    var Resto;
+    Soma = 0;
+    if (strCPF === "00000000000") return false;
+
+    for (let i = 1; i <= 9; i++) Soma = Soma + parseInt(strCPF.substring(i - 1, i)) * (11 - i);
+    Resto = (Soma * 10) % 11;
+
+    if ((Resto === 10) || (Resto === 11)) Resto = 0;
+    if (Resto !== parseInt(strCPF.substring(9, 10))) return false;
+
+    Soma = 0;
+    for (let i = 1; i <= 10; i++) Soma = Soma + parseInt(strCPF.substring(i - 1, i)) * (12 - i);
+    Resto = (Soma * 10) % 11;
+
+    if ((Resto === 10) || (Resto === 11)) Resto = 0;
+    if (Resto !== parseInt(strCPF.substring(10, 11))) return false;
+    return true;
+  }
+
+  validarCNPJ = async (event) => {
+    let ok = false, msg = '';
+    let val = event.target.value.replace(/\D/g, '');
+
+    if (val.length < 14) {
+      msg = 'Formato inválido';
+    }
+
+    else if (!this.testarCNPJ(val)) {
+      msg = 'CNPJ incorreto';
+    }
+
+    else {
+      ok = true;
+    }
+
+    let newState = Object.assign({}, this.state.validacao);
+    newState.cnpj.ok = ok;
+    newState.cnpj.msg = msg;
+    this.setState({ validacao: newState });
+
+    if (val.length === 14) {
+      let dados = await serverRequest.request('/restaurante/checarSeCNPJExiste', { cnpj: val });
+      if (dados.exists) {
+        let newState = Object.assign({}, this.state.validacao);
+        newState.cnpj.ok = false;
+        newState.cnpj.msg = 'Este CNPJ já está cadastrado';
+        this.setState({ validacao: newState });
+      }
+    }
+
+  }
+
+  testarCNPJ = (cnpj) => {
+
+    if (cnpj === '') return false;
+
+    if (cnpj.length !== 14)
+      return false;
+
+    // Elimina CNPJs invalidos conhecidos
+    if (cnpj === "00000000000000" ||
+      cnpj === "11111111111111" ||
+      cnpj === "22222222222222" ||
+      cnpj === "33333333333333" ||
+      cnpj === "44444444444444" ||
+      cnpj === "55555555555555" ||
+      cnpj === "66666666666666" ||
+      cnpj === "77777777777777" ||
+      cnpj === "88888888888888" ||
+      cnpj === "99999999999999")
+      return false;
+
+    // Valida DVs
+    let tamanho = cnpj.length - 2
+    let numeros = cnpj.substring(0, tamanho);
+    let digitos = cnpj.substring(tamanho);
+    let soma = 0;
+    let pos = tamanho - 7;
+
+    for (let i = tamanho; i >= 1; i--) {
+      soma += numeros.charAt(tamanho - i) * pos--;
+      if (pos < 2)
+        pos = 9;
+    }
+    let resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
+    if (resultado.toString() !== digitos.charAt(0))
+      return false;
+    tamanho = tamanho + 1;
+    numeros = cnpj.substring(0, tamanho);
+    soma = 0;
+    pos = tamanho - 7;
+    for (let i = tamanho; i >= 1; i--) {
+      soma += numeros.charAt(tamanho - i) * pos--;
+      if (pos < 2)
+        pos = 9;
+    }
+    resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
+    if (resultado.toString() !== digitos.charAt(1))
+      return false;
+
+    return true;
+  }
+
+
 
   prosseguir = (event) => {
     event.preventDefault();
@@ -163,9 +326,16 @@ class Step3 extends Component {
             value="1"
             checked={this.state.id_tipo_cadastro_conta === "1" ? true : false}
             onChange={this.changeInput}
-            disabled={this.state.pagamento_app ? false : true} />
+            onBlur={this.validarCPF}
+            disabled={this.state.pagamento_app ? false : true}
+            guide={true}
+            invalid={!this.state.validacao.cpf_administrador.ok}
+            required
+          />
+          <FormFeedback invalid>{this.state.validacao.cpf_administrador.msg}</FormFeedback>
           <Label className="form-check-label" check htmlFor="cpf-radio">CPF</Label>
         </FormGroup>
+        
         <FormGroup check inline>
           <Input
             className="form-check-input"
@@ -175,7 +345,13 @@ class Step3 extends Component {
             value="2"
             checked={this.state.id_tipo_cadastro_conta === "2" ? true : false}
             onChange={this.changeInput}
-            disabled={this.state.pagamento_app ? false : true} />
+            onBlur={this.validarCNPJ}
+            disabled={this.state.pagamento_app ? false : true}
+            guide={true}
+            invalid={!this.state.validacao.cnpj.ok}
+            required
+          />
+          <FormFeedback invalid>{this.state.validacao.cnpj.msg}</FormFeedback>
           <Label className="form-check-label" check htmlFor="cnpj-radio">CNPJ</Label>
         </FormGroup>
 
