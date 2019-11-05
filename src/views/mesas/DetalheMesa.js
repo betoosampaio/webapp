867f3416061d7c1570ddf923f4371d2714a3f505
iagoguimaraes <iagog6@hotmail.com>
2019-11-04 23:06:25 -0300

    lista de pagamento dinamica layout redesenhado

--- src/views/mesas/DetalheMesa.js
+++ src/views/mesas/DetalheMesa.js
@@ -14,6 +14,7 @@ class DetalheMesa extends Component {
     super(props);
     this.state = {
       produtos: [],
+      pagamentos: [],
       id_mesa: "",
     };
   }
@@ -33,7 +34,7 @@ class DetalheMesa extends Component {
     let vl = 0;
     vl = this.state.produtos.reduce((sum, key) =>
       sum + (key.removido ? 0 : key.preco * key.quantidade), 0)
-    return `R$ ${vl.toFixed(2)}`;
+    return vl;
   }
 
   qtdProdutos = () => {
@@ -43,15 +44,15 @@ class DetalheMesa extends Component {
     return qt;
   }
 
-  vlrServicos = () => {
-    let
-      txservico = parseFloat(this.state.taxa_servico) || 0,
-      desconto = parseFloat(this.state.desconto) || 0;
-    return `R$ ${(txservico - desconto).toFixed(2)}`;
+  vlrPagamentos = () => {
+    let vl = 0;
+    vl = this.state.pagamentos.reduce((sum, key) =>
+      sum + (key.removido ? 0 : key.valor), 0)
+    return vl;
   }
 
-  vlrTxServico = () => { return `R$ ${(parseFloat(this.state.taxa_servico) || 0).toFixed(2)}` }
-  vlrDesconto = () => { return `R$ -${(parseFloat(this.state.desconto) || 0).toFixed(2)}` }
+  vlrTxServico = () => { return parseFloat(this.state.taxa_servico) || 0}
+  vlrDesconto = () => { return parseFloat(this.state.desconto) || 0 }
 
   vlrTotal = () => {
     let
@@ -61,7 +62,15 @@ class DetalheMesa extends Component {
 
     vl = this.state.produtos.reduce((sum, key) =>
       sum + (key.removido ? 0 : key.preco * key.quantidade), 0)
-    return `R$ ${(vl + txservico - desconto).toFixed(2)}`;
+    return (vl + txservico - desconto);
+  }
+
+  vlrRestante = () => {
+    return this.vlrTotal() - this.vlrPagamentos();
+  }
+
+  moneyFormat = (valor) => {
+    return `R$ ${valor.toFixed(2)}`;
   }
 
   itemIncluso = () => {
@@ -127,7 +136,31 @@ class DetalheMesa extends Component {
       <div>
         <h2 className="mb-4">Mesa {this.state.numero}</h2>
         <Row>
-          <Col xs="12" sm="6" lg="4">
+          <Col xs="12" sm="6" lg="3">
+            <Card className="text-white bg-secondary">
+              <CardBody>
+                <div className="text-value">{this.moneyFormat(this.vlrTotal())}</div>
+                <div>Valor Total</div>
+              </CardBody>
+            </Card>
+          </Col>
+          <Col xs="12" sm="6" lg="3">
+            <Card className="text-white bg-success">
+              <CardBody>
+                <div className="text-value">{this.moneyFormat(this.vlrPagamentos())}</div>
+                <div>Valor Pago</div>
+              </CardBody>
+            </Card>
+          </Col>
+          <Col xs="12" sm="6" lg="3">
+            <Card className="text-white bg-primary">
+              <CardBody>
+                <div className="text-value">{this.moneyFormat(this.vlrRestante())}</div>
+                <div>Valor Restante</div>
+              </CardBody>
+            </Card>
+          </Col>
+          <Col xs="12" sm="6" lg="3">
             <Card>
               <CardBody>
                 <Button
@@ -149,18 +182,75 @@ class DetalheMesa extends Component {
               </CardBody>
             </Card>
           </Col>
-
-          <Col xs="12" sm="6" lg="4">
-            <Card className="text-white bg-success">
-              <CardBody>
-                <div className="text-value">{this.vlrTotal()}</div>
-                <div>Valor Total</div>
-              </CardBody>
-            </Card>
-          </Col>
-
         </Row>
         <Row>
+          <Col xs={12} md={4} lg={5}>
+            <Card>
+              <CardHeader><i className='icon-calculator'></i>Resumo
+              </CardHeader>
+              <CardBody>
+                <ListGroup>
+                  <ListGroupItem><i className="fa fa-wrench mr-2 text-muted" />Taxa de Serviço
+                    <Button size="sm" className="pull-right">{this.moneyFormat(this.vlrTxServico())}</Button>
+                  </ListGroupItem>
+                  <ListGroupItem><i className="fa fa-dollar mr-2 text-muted" />Desconto
+                    <Button size="sm" className="pull-right">{this.moneyFormat(this.vlrDesconto()*-1)}</Button>
+                  </ListGroupItem>
+                  <ListGroupItem><i className="fa fa-cutlery mr-2 text-muted" />Produtos
+                    <span className="pull-right">{this.moneyFormat(this.vlrProdutos())}</span>
+                  </ListGroupItem>
+                </ListGroup>
+              </CardBody>
+              <CardFooter>
+                <b>Total</b>
+                <b className="pull-right">{this.vlrTotal()}</b>
+              </CardFooter>
+            </Card>
+
+            <Card>
+              <CardHeader><i className='fa fa-money'></i>
+                Pagamentos
+                <Button className="pull-right" color="success" size="sm">
+                  <i className="icon-plus"></i>&nbsp;Incluir
+                </Button>
+              </CardHeader>
+              <CardBody>
+                <Table striped bordered hover responsive>
+                  <thead>
+                    <tr>
+                      <th>Forma</th>
+                      <th>Valor</th>
+                      <th>Excluir</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {
+                      this.state.pagamentos.map((obj) => {
+                        return (
+                          <tr key={obj.data_inclusao} style={{ textDecoration: obj.removido ? "line-through" : "none" }}>
+                            <td>{obj.ds_forma_pagamento}</td>
+                            <td>{this.moneyFormat(obj.valor)}</td>
+                            <td>
+                              {obj.removido
+                                ? null
+                                : <Button color="danger" size="sm">
+                                  <i className="icon-close"></i>
+                                </Button>
+                              }
+                            </td>
+                          </tr>
+                        );
+                      })
+                    }
+                  </tbody>
+                </Table>
+              </CardBody>
+              <CardFooter>
+                <b>Total</b>
+                <b className="pull-right">{this.moneyFormat(this.vlrPagamentos())}</b>
+              </CardFooter>
+            </Card>
+          </Col>
           <Col xs={12} md={8} lg={7}>
             <Card>
               <CardHeader>
@@ -188,7 +278,7 @@ class DetalheMesa extends Component {
                             <td><Foto src={obj.imagem} height="30" width="30"></Foto></td>
                             <td>{obj.nome_produto}</td>
                             <td>{obj.quantidade}</td>
-                            <td>{`R$ ${(obj.preco * obj.quantidade).toFixed(2).replace('.', ',')}`}</td>
+                            <td>{this.moneyFormat(obj.preco * obj.quantidade)}</td>
                             <td>
                               {obj.removido
                                 ? null
@@ -206,65 +296,11 @@ class DetalheMesa extends Component {
               </CardBody>
               <CardFooter>
                 <b>{this.qtdProdutos()} items</b>
-                <b className="pull-right">{this.vlrProdutos()}</b>
+                <b className="pull-right">{this.moneyFormat(this.vlrProdutos())}</b>
               </CardFooter>
             </Card>
           </Col>
-          <Col xs={12} md={4} lg={5}>
 
-            <Card>
-              <CardHeader><i className='icon-calculator'></i>Resumo
-              </CardHeader>
-              <CardBody>
-                <ListGroup>
-                  <ListGroupItem><i className="fa fa-wrench mr-2 text-muted" />Taxa de Serviço
-                    <Button size="sm" className="pull-right">{this.vlrTxServico()}</Button>
-                  </ListGroupItem>
-                  <ListGroupItem><i className="fa fa-dollar mr-2 text-muted" />Desconto
-                    <Button size="sm" className="pull-right">{this.vlrDesconto()}</Button>
-                  </ListGroupItem>
-                  <ListGroupItem><i className="fa fa-cutlery mr-2 text-muted" />Produtos
-                    <span className="pull-right">{this.vlrProdutos()}</span>
-                  </ListGroupItem>
-                </ListGroup>
-              </CardBody>
-              <CardFooter>
-                <b>Total</b>
-                <b className="pull-right">{this.vlrTotal()}</b>
-              </CardFooter>
-            </Card>
-
-            <Card>
-              <CardHeader><i className='fa fa-money'></i>
-                Pagamentos
-                <Button className="pull-right" color="success" size="sm">
-                  <i className="icon-plus"></i>&nbsp;Incluir
-                </Button>
-              </CardHeader>
-              <CardBody>
-                <Table striped bordered hover responsive>
-                  <thead>
-                    <tr>
-                      <th>Forma</th>
-                      <th>Valor</th>
-                      <th>Excluir</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr>
-                      <td>Dinheiro</td>
-                      <td>R$ 50,00</td>
-                      <td><Button color="danger" size="sm"><i className="icon-close"></i></Button></td>
-                    </tr>
-                  </tbody>
-                </Table>
-              </CardBody>
-              <CardFooter>
-                <b>Total</b>
-                <b className="pull-right">R$ 50,00</b>
-              </CardFooter>
-            </Card>
-          </Col>
         </Row>
         <IncluirItem
           show={this.state.modalAdicionarItem}
