0fac7c9180278133ac78bc0ff9367151b85e1a60
Brendon Guedes <lordbguedes@gmail.com>
2019-11-29 19:09:36 -0300

    check mostrar caixas abertos

--- src/views/caixa/ListaCaixa.js
+++ src/views/caixa/ListaCaixa.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
-import { Button, Card, CardHeader, CardBody } from 'reactstrap';
+import {
+  Button, Card, CardHeader, CardBody, FormGroup, Label, InputGroup, InputGroupAddon,
+  InputGroupText, Collapse, Form, CustomInput
+} from 'reactstrap';
 import serverRequest from '../../utils/serverRequest';
 import ReactTable from 'react-table';
 import 'react-table/react-table.css';
 import IncluirCaixa from './IncluirCaixa';
 import { Link } from 'react-router-dom';
+import moment from 'moment';
+import DatePicker from 'react-datepicker';
+import "react-datepicker/dist/react-datepicker.css";
 
 class ListaCaixa extends Component {
 
@@ -13,6 +19,10 @@ class ListaCaixa extends Component {
 
     this.state = {
       caixas: [],
+      dtini: new Date(),
+      dtfim: new Date(),
+      showFiltros: true,
+      somenteAberto: true,
     };
   }
 
@@ -58,10 +68,25 @@ class ListaCaixa extends Component {
   }
 
   obterLista = async () => {
-    let dados = await serverRequest.request('/caixa/listar');
-    if (dados) {
-      this.setState({ caixas: dados });
+    console.log(this.state.somenteAberto);
+    if (this.state.somenteAberto) {
+      let dados = await serverRequest.request('/caixa/listar');
+      if (dados) {
+        this.setState({ caixas: dados });
+      }
+
     }
+
+    else {
+      let dados = await serverRequest.request('/caixa/consultar', {
+        dtini: moment(this.state.dtini).format('YYYY-MM-DDT00:00:00.000Z'),
+        dtfim: moment(this.state.dtfim).format('YYYY-MM-DDT23:59:59.999Z'),
+      });
+      if (dados) {
+        this.setState({ caixas: dados });
+      }
+    }
+
   }
 
   caixaAdicionado = () => {
@@ -69,10 +94,97 @@ class ListaCaixa extends Component {
     this.obterLista();
   }
 
+  consultar = async (event) => {
+    event.preventDefault();
+    this.obterLista();
+  }
+
+  changeSwitch = (event) => {
+    this.setState({ [event.target.name]: event.target.checked ? 1 : 0 });
+  }
+
+  dateFormat = (data) => {
+    let dataRetornar = new Date(data).toLocaleString();
+    return dataRetornar;
+  }
+
   render() {
+
+    let caixas = this.state.caixas
+    if (this.state.somenteAbertos) caixas = caixas.filter(row => row.status);
+    if (this.state.search) {
+      caixas = caixas.filter(row => {
+        return (new RegExp(this.state.search, "i")).test([row.status].join(''))
+      })
+    }
+    if (this.state.showFiltros) {
+      caixas = caixas.filter(row => String(row.status) === String(this.state.showFiltros))
+    }
+
     return (
       <div>
-
+        <Card>
+          <CardHeader>
+            <i className='fa fa-calendar' />Período
+              <div className="card-header-actions">
+              <div onClick={() => { this.setState({ showFiltros: !this.state.showFiltros }); }}>
+                <Button size="sm" color="secondary">
+                  <i className={this.state.showFiltros ? "fa fa-caret-up" : "fa fa-caret-down"} />
+                </Button>
+              </div>
+            </div>
+          </CardHeader>
+          <CardBody>
+          </CardBody>
+          <Collapse isOpen={this.state.showFiltros}>
+            <CardBody>
+              <Form inline onSubmit={this.consultar}>
+                <FormGroup>
+                  <Label className="mr-2">Somente caixas abertos:</Label>
+                  <CustomInput
+                    id="somenteAberto"
+                    type="switch"
+                    name="somenteAberto"
+                    checked={this.state.somenteAberto ? true : false}
+                    onChange={this.changeSwitch}
+                    valid
+                  />
+                </FormGroup>
+                {!this.state.somenteAberto === true &&
+                  <FormGroup className="mr-2">
+                    <Label className="mr-2">Data Inicial:</Label>
+                    <InputGroup>
+                      <InputGroupAddon addonType="append">
+                        <InputGroupText><i className="icon-calendar"></i></InputGroupText>
+                      </InputGroupAddon>
+                      <DatePicker
+                        className="form-control w-100"
+                        selected={this.state.dtini}
+                        onChange={date => this.setState({ dtini: date })}
+                        dateFormat="dd/MM/yyyy" />
+                    </InputGroup>
+                  </FormGroup>
+                }
+                {!this.state.somenteAberto === true &&
+                  <FormGroup className="mr-3">
+                    <Label className="mr-2">Data Final:</Label>
+                    <InputGroup>
+                      <InputGroupAddon addonType="append">
+                        <InputGroupText><i className="icon-calendar"></i></InputGroupText>
+                      </InputGroupAddon>
+                      <DatePicker
+                        className="form-control"
+                        selected={this.state.dtfim}
+                        onChange={date => this.setState({ dtfim: date })}
+                        dateFormat="dd/MM/yyyy" />
+                    </InputGroup>
+                  </FormGroup>
+                }
+                <Button type="submit" className="ml-auto" color="success"><i className="fa fa-search mr-1"></i>Consultar</Button>
+              </Form>
+            </CardBody>
+          </Collapse>
+        </Card>
         <p></p>
         <Card>
           <CardHeader>
@@ -109,3 +221,5 @@ class ListaCaixa extends Component {
 }
 
 export default ListaCaixa;
+
+
