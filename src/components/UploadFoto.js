d5cb7fb74c157dcf10b18df8f713b00bc3c9c17a
DiegoHonorio <honorioproduction@gmail.com>
2019-12-02 19:02:28 -0300

    Sync Inf FOTos

---
+++ src/components/UploadFoto.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import serverRequest from '../utils/serverRequest';
+import FotoProduto from './FotoProduto';
+
+class UploadFotoProduto extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      path: ""
+    }
+  }
+
+  upload = async (event) => {
+    let name = event.target.name;
+
+    let form = new FormData();
+    form.append('imagem', event.target.files[0]);
+
+    let dados = await serverRequest.requestForm('/produto/uploadimg', form);
+
+    this.props.onChange({ target: { name: name, value: dados } });
+    this.setState({ path: dados });
+
+  }
+
+  remover = (name) => {
+    this.props.onChange({ target: { name: name, value: "" } });
+    this.setState({ path: "" });
+  }
+
+  render() {
+    const styleFileUpload = { border: "1px solid #ccc", display: "inline-block", padding: "6px 12px", cursor: "pointer" }
+    const name = this.props.name;
+    return (
+      <div>
+        <label htmlFor="imgupload" style={styleFileUpload}>
+          <i className="fa fa-cloud-upload"></i> Escolher Arquivo
+        </label>
+        <input {...this.props} id="imgupload" type="file" onChange={this.upload} style={{ display: "none" }} />
+        <label style={styleFileUpload} onClick={() => { this.remover(name) }}>
+          <i className="fa fa-remove"></i>
+        </label>
+        <br />
+        {
+          (this.state.path || this.props.path)
+            ? <FotoProduto src={this.state.path || this.props.path} alt=""></FotoProduto>
+            : <span />
+        }
+      </div>
+
+    )
+  }
+}
+
+export default UploadFotoProduto;
