ff20a62e740f5cf55debf30928607500a3410cca
Brendon Guedes <lordbguedes@gmail.com>
2019-12-10 16:55:47 -0300

    iniciando criar tela de controle de ambientes

--- src/views/controle/Controle.js
+++ src/views/controle/Controle.js
@@ -1,22 +1,92 @@
 import React, { Component } from 'react';
+import {
+	Card, CardHeader, CardBody, Button, FormGroup, Label, Input, InputGroup,
+	InputGroupAddon, InputGroupText
+} from 'reactstrap';
+import Tabs from 'react-bootstrap/Tabs';
+import Tab from 'react-bootstrap/Tab';
+import { AppSwitch } from '@coreui/react'
+import serverRequest from '../../utils/serverRequest';
+import Modal from 'react-bootstrap/Modal';
+import Ambiente from '../ambiente/Ambiente';
 
-class Controle extends Component {
+class EditarPerfil extends Component {
 
-  constructor(props) {
-    super(props);
+	constructor(props) {
+		super(props);
+		this.state = {
+      showConfirm: false,		
+      ds_ambiente: "",	
+		};
+	}
 
-    this.state = {
-      lista: [],
-    };
-  }
+	componentDidMount() {
+		this.obter(this.props.match.params.id);
+	}
 
-  render() {
-    return (
-      <>
-        teste
-      </>
-    );
-  }
+	obter = async (id) => {
+		let dados = await serverRequest.request('/ambiente/obter', { "id_ambiente": id });
+		if (dados) {
+			this.setState(dados[0]);
+		}
+	}
+
+	editar = async (event) => {
+		event.preventDefault();
+		let dados = await serverRequest.request('/ambientel/editar', this.state);
+		if (dados) {
+			window.location.href = '#/ambiente';
+		}
+	}
+
+	changeInput = (event) => {
+		this.setState({ [event.target.name]: event.target.value });
+	}
+
+	changeSwitch = (event) => {
+		this.setState({ [event.target.name]: event.target.checked ? 1 : 0 });
+	}
+
+	render() {
+		return (
+			<div>
+				<Tabs defaultActiveKey="ambiente">
+					<Tab eventKey="ambiente" title="Ambiente">
+						<Card>
+							<CardHeader>
+								<strong>Editar Perfil</strong>
+							</CardHeader>
+							<CardBody>
+								<FormGroup>
+									<Label>Perfil:</Label>
+									<InputGroup>
+										<InputGroupAddon addonType="append">
+											<InputGroupText><i className="fa fa-pencil"></i></InputGroupText>
+										</InputGroupAddon>
+										<Input name="ds_ambiente" value={this.state.ds_ambiente} onChange={this.changeInput} required minLength="4" placeholder="Garçom" />
+									</InputGroup>
+								</FormGroup>
+								<FormGroup>
+									<Label>Ativo:</Label>
+									<InputGroup>
+										<AppSwitch name="ativo" className={'mx-1'} variant={'pill'} color={'success'} checked={this.state.ativo ? true : false} onChange={this.changeSwitch} />
+									</InputGroup>
+								</FormGroup>
+
+							</CardBody>
+							<Modal.Footer>
+								<Button variant="primary" color="danger" onClick={() => { window.location.href = '#/ambiente' }} >Cancelar</Button>
+								<Button variant="primary" color="success" onClick={this.editar}  >Confirmar</Button>
+							</Modal.Footer>
+						</Card>
+					</Tab>
+					<Tab eventKey="ambiente" title="Ambiente">
+						<Ambiente id_ambiente={this.props.match.params.id}/>
+					</Tab >
+				</Tabs>
+			</div>
+		);
+	}
 }
 
-export default Controle;
+export default EditarPerfil;
