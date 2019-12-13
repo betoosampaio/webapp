e40573c20cfa8999284324a47062e0af3c75eda3
Brendon Guedes <lordbguedes@gmail.com>
2019-12-13 17:38:57 -0300

    listar itens pendentes de preparo

---
+++ src/views/controle/CardControle.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import { Card, CardHeader, CardBody } from 'reactstrap';
+
+class CardControle extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+  dateDiff = (dataIni, dataFim) => {
+    let delta = Math.abs(dataIni - dataFim) / 1000;
+
+    let d = Math.floor(delta / 86400);
+    delta -= d * 86400;
+
+    let h = Math.floor(delta / 3600) % 24;
+    delta -= h * 3600;
+
+    let m = Math.floor(delta / 60) % 60;
+    delta -= m * 60;
+
+    return ` ${d ? d + "d" : ""} ${h ? h + "h" : ""} ${m}m`;
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(() => this.setState({ time: Date.now() }), 3000);
+  }
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  render() {
+
+    return (
+
+      <Card>
+        <CardHeader>
+          <i className="icon-note"></i>
+          <span className="font-lg font-weight-bold">Mesa {this.props.pedido.numero}</span>
+        </CardHeader>
+        <CardBody>
+          <div>Produto: {this.props.pedido.produtos.nome_produto}
+          </div>
+          <div className="font-xs">
+            <i className="fa fa-clock-o"></i>
+            {this.dateDiff(new Date(this.props.pedido.produtos.data_incluiu), new Date())}
+          </div>
+        </CardBody>
+      </Card>
+    )
+  }
+}
+
+export default CardControle;
---
+++ src/views/controle/ListaControle.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import {
+  Card, CardBody, Button, Row, Col, ListGroupItem, ListGroup, ButtonDropdown,
+  ButtonGroup, DropdownItem, DropdownMenu, DropdownToggle, Modal
+} from 'reactstrap';
+import serverRequest from '../../utils/serverRequest';
+import 'react-toastify/dist/ReactToastify.css';
+import CardControle from './CardControle';
+
+
+class ListaControle extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      modalInfoProduto: false,
+      id_produto: "",     
+      lista: [],
+
+
+    };
+  }
+
+  componentDidMount() {
+    this.obterLista();
+  }
+
+  obterLista = async () => {
+    let dados = await serverRequest.request('/mesa/item/listaPrepararAmbiente', ({ id_ambiente: 1 }));
+    if (dados) {
+      this.setState({ lista: dados });
+    }
+  }
+
+
+  dateFormat = (data) => {
+    let dataRetornar = new Date(data).toLocaleString();
+    return dataRetornar;
+
+  }
+
+  render() {
+
+
+    return (
+      <>
+       
+        <Row size="sm" >
+          Escolha um ambiente:
+        </Row>
+        <p></p>
+        <Row>
+          {
+            this.state.lista.map((obj) => {
+              return (
+                <Col xs="12" sm="6" lg="3" key={obj.id_item}>
+                  <CardControle pedido={obj} atualizar={this.obterLista} />
+                </Col>
+              );
+            })
+          }
+        </Row>
+      </>
+    );
+  }
+}
+
+export default ListaControle;
